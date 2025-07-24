ce22711de45119d79d433f58964a42e7c85c6c16
rivazahra <riva.zahra88@gmail.com>
2025-07-24 18:40:12 +0700

    Feat search logic to medical records

--- src/pages/MedicalRecords.jsx
+++ src/pages/MedicalRecords.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { IoSearchOutline } from 'react-icons/io5'
 import CardMedicalRecords from '../components/CardMedicalRecords'
 
 const MedicalRecords = () => {
   const pasien = JSON.parse(localStorage.getItem('dataHewan'))
-  const medicalRecords = JSON.parse(localStorage.getItem('rekamMedis'))
+  const rekamMedis = JSON.parse(localStorage.getItem('rekamMedis'))
 
-  const [rekamMedis, setRekamMedis] = useState(medicalRecords)
+  const [searchValue, setsearchValue] = useState('')
+
 
   const [formData, setFormData] = useState({
     pasien: '',
@@ -28,7 +29,30 @@ const MedicalRecords = () => {
     }))
   }
 
-  const [searchValue, setsearchValue] = useState([])
+  const handleSearch = useMemo(() => {
+    let filtered = rekamMedis;
+
+    if (searchValue.trim()) {
+      const searchLower = searchValue.toLowerCase().trim()
+      filtered = filtered.filter((patient) => patient.pasien?.toLowerCase().includes(searchLower))
+    }
+    return filtered
+  }, [rekamMedis, searchValue])
+
+  const handleReset = () => {
+    setFormData({
+      pasien: '',
+      tglKunjungan: '',
+      keluhan: '',
+      diagnosis: '',
+      penanganan: '',
+      obat: '',
+      biayaPengobatan: '',
+      kunjunganBerikutnya: '',
+      catatan: '',
+    })
+  }
+
 
   const saveForm = (e) => {
     e.preventDefault()
@@ -67,18 +91,19 @@ const MedicalRecords = () => {
   }
 
   return (
-    <div className="medical-records flex flex-col gap-8">
+    <div className="medical-records flex flex-col gap-8 text-sm md:text-md">
       <div className="information space-y-2">
         <h1 className="text-3xl font-bold capitalize">Rekam Medis</h1>
         <p className="">Tambah dan kelola rekam medis pasien</p>
       </div>
       <div className="information space-y-5">
         <h1 className="text-2xl font-semibold mb-5">Tambah rekam medis baru</h1>
-        <form action="" onSubmit={saveForm}>
+
+        <form action="med-records" onSubmit={saveForm}>
           <div className="grid grid-cols-2 gap-3">
             <div className="form-group">
-              <label className="font-bold">Pilih pasien</label>
-              <select className="form-control" name="pasien" value={formData.pasien} onChange={handleChange}>
+              <label htmlFor='pilih-pasien' className="font-bold">Pilih pasien</label>
+              <select className="form-control" id='pilih-pasien' name="pasien" value={formData.pasien} onChange={handleChange}>
                 <option value="">Pilih pasien..</option>
                 {pasien.map((pas) => (
                   <option key={pas.id} value={pas.pasien}>
@@ -89,53 +114,56 @@ const MedicalRecords = () => {
             </div>
 
             <div className="form-group">
-              <label>Tanggal Kunjungan</label>
-              <input type="date" name="tglKunjungan" onChange={handleChange} value={formData.tglKunjungan} className="form-control" />
+              <label htmlFor='tgl-kunjungan'>Tanggal Kunjungan</label>
+              <input type="date" id='tgl-kunjungan' name="tglKunjungan" onChange={handleChange} value={formData.tglKunjungan} className="form-control" />
             </div>
           </div>
           <div className="grid grid-cols-1 gap-3 mt-3">
             <div className="form-group">
-              <label>Keluhan/Gejala</label>
-              <textarea className="h-20 form-control" onChange={handleChange} name="keluhan" value={formData.keluhan} placeholder="Masukkan keluhan atau gejala yang dialami hewan"></textarea>
+              <label htmlFor='keluhan-gejala'>Keluhan/Gejala</label>
+              <textarea className="h-20 form-control" id='keluhan-gejala' onChange={handleChange} name="keluhan" value={formData.keluhan} placeholder="Masukkan keluhan atau gejala yang dialami hewan"></textarea>
             </div>
             <div className="form-group">
-              <label>Diagnosis</label>
-              <textarea className="h-20 form-control" name="diagnosis" value={formData.diagnosis} placeholder="Masukkan diagnosis dari dokter hewan" onChange={handleChange}></textarea>
+              <label htmlFor='diagnosis'>Diagnosis</label>
+              <textarea id='diagnosis' className="h-20 form-control" name="diagnosis" value={formData.diagnosis} placeholder="Masukkan diagnosis dari dokter hewan" onChange={handleChange}></textarea>
             </div>
           </div>
 
           {/* Grid kembali ke 2 kolom */}
-          <div className="grid grid-cols-2 gap-3 mt-3">
+          <div className="grid grid-cols-2  md:gap-3 mt-3 ">
             <div className="form-group">
-              <label>Penanganan/Pengobatan</label>
-              <textarea className="h-20 form-control" name="penanganan" value={formData.penanganan} placeholder="Masukkan detail penanganan atau pengobatan" onChange={handleChange}></textarea>
+              <label className='' htmlFor='penanganan'>Penanganan/Pengobatan</label>
+              <textarea id='penanganan' className="h-20 form-control txt-area" name="penanganan" value={formData.penanganan} placeholder="Masukkan detail penanganan atau pengobatan" onChange={handleChange}></textarea>
             </div>
 
             <div className="form-group">
-              <label>Obat yang diberikan</label>
-              <textarea className="h-20 form-control" name="obat" value={formData.obat} placeholder="Masukkan obat beserta dosis" onChange={handleChange}></textarea>
+              <label htmlFor='obat-pasien'>Obat yang diberikan</label>
+              <textarea className="h-20 form-control" id='obat-pasien' name="obat" value={formData.obat} placeholder="Masukkan obat beserta dosis" onChange={handleChange}></textarea>
             </div>
             <div className="form-group">
-              <label>Kunjungan Berikutnya (jika ada)</label>
-              <input type="date" name="kunjunganBerikutnya" value={formData.kunjunganBerikutnya} className="form-control" onChange={handleChange} />
+              <label htmlFor='nextKunjungan'>Kunjungan Berikutnya (jika ada)</label>
+              <input type="date" id='nextKunjungan' name="kunjunganBerikutnya" value={formData.kunjunganBerikutnya} className="form-control" onChange={handleChange} />
             </div>
 
             <div className="form-group">
-              <label>Biaya Pengobatan</label>
-              <input type="number" name="biayaPengobatan" value={formData.biayaPengobatan} className="form-control" placeholder="Masukkan biaya dalam rupiah, contoh: 35,000" onChange={handleChange} />
+              <label htmlFor='biaya-pengobatan'>Biaya Pengobatan</label>
+              <input type="number" id='biaya-pengobatan' name="biayaPengobatan" value={formData.biayaPengobatan} className="h-10 form-control" placeholder="Masukkan biaya dalam rupiah, contoh: 35,000" onChange={handleChange} />
             </div>
           </div>
           <div className="grid grid-cols-1 ">
             <div className="form-group">
-              <label htmlFor="">Catatan tambahan</label>
-              <textarea name="catatan" value={formData.catatan} id="" className="form-control" placeholder="Catatan khusus dari dokter hewan (opsional)" onChange={handleChange}></textarea>
+              <label htmlFor="catatanTambahan">Catatan tambahan</label>
+              <textarea name="catatan" value={formData.catatan} id="catatanTambahan" className="form-control" placeholder="Catatan khusus dari dokter hewan (opsional)" onChange={handleChange}></textarea>
             </div>
           </div>
           <button className="button-save" type="submit">
             Simpan
           </button>
-          <button className="button-reset">Reset</button>
+          <button className="btn-reset" type='button' onClick={handleReset}>Reset</button>
         </form>
+        <div>
+
+        </div>
       </div>
       <div className="information space-y-3">
         <h1 className="font-bold text-2xl">Riwayat rekam medis</h1>
@@ -143,7 +171,7 @@ const MedicalRecords = () => {
           <i>
             <IoSearchOutline />
           </i>
-          <input type="text" value={searchValue} name="search" onChange={(e) => setsearchValue(e.target.value)} className="input-search" placeholder="Cari berdasarkan nama hewan, atau nama pemilik" />
+          <input type="text" value={searchValue} name="search" onChange={(e) => setsearchValue(e.target.value)} className="input-search" placeholder="Cari berdasarkan nama hewan.." />
         </div>
         {rekamMedis?.length == 0 || rekamMedis == null ? (
 
@@ -152,8 +180,8 @@ const MedicalRecords = () => {
           </p>
         )
           : (
-            rekamMedis?.map((rekam) => (
-           <CardMedicalRecords rekam={rekam} key={rekam.id}/>
+            handleSearch?.map((rekam) => (
+              <CardMedicalRecords rekam={rekam} key={rekam.id} />
             ))
           )}
 
