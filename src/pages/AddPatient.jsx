d96e2516640bda4e19e52d92817aade305b4ea67
rivazahra <riva.zahra88@gmail.com>
2025-07-28 09:00:56 +0700

    Feat htmlFor to all label

--- src/pages/AddPatient.jsx
+++ src/pages/AddPatient.jsx
@@ -59,7 +59,7 @@ const AddPatient = () => {
 
 
   return (
-    <form action="" onSubmit={saveForm}>
+    <form id='add-patient' action="" onSubmit={saveForm}>
       <div className="add-patient">
         <div className="flex  flex-col gap-10">
           <div className="information space-y-5">
@@ -70,38 +70,40 @@ const AddPatient = () => {
             <h1 className="font-bold text-2xl">Informasi Pasien</h1>
             <div className="grid grid-cols-2 gap-3">
               <div className="form-group">
-                <label htmlFor="">Nama Hewan</label>
-                <input type="text" name="name" value={formData.name} onChange={handleChange} className="form-control" placeholder="Masukkan nama hewan" />
+                <label htmlFor="nama-hewan">Nama Hewan</label>
+                <input type="text" id='nama-hewan' name="name" value={formData.name} onChange={handleChange} className="form-control" placeholder="Masukkan nama hewan" />
               </div>
               <div>
                 <div className="form-group">
-                  <label htmlFor="">Jenis Hewan</label>
+                  <label htmlFor="jenis-hewan">Jenis Hewan</label>
                   <select id="jenis-hewan" className="form-control" name="jenis" value={formData.jenis} onChange={handleChange}>
                     <option value="">Pilih jenis hewan</option>
                     <option value="kucing">Kucing</option>
                     <option value="anjing">Anjing</option>
+                    <option value="ikan">Ikan</option>
+                    <option value="kelinci">Kelinci</option>
                   </select>
                 </div>
               </div>
               <div className="form-group">
-                <label htmlFor="">Ras</label>
-                <input type="text" className="form-control" value={formData.ras} onChange={handleChange} name="ras" placeholder="Contoh: Persia" />
+                <label htmlFor="ras">Ras</label>
+                <input type="text" id='ras' className="form-control" value={formData.ras} onChange={handleChange} name="ras" placeholder="Contoh: Persia" />
               </div>
               <div className="form-group">
-                <label htmlFor="">Umur</label>
-                <input type="text" className="form-control" onChange={handleChange} name="umur" placeholder="Masukkan umur hewan" value={formData.umur} />
+                <label htmlFor="umur">Umur</label>
+                <input type="text" id='umur' className="form-control" onChange={handleChange} name="umur" placeholder="Masukkan umur hewan" value={formData.umur} />
               </div>
               <div className="form-group">
-                <label htmlFor="">Jenis kelamin</label>
-                <select name="jenisKelamin" id="" className="form-control" onChange={handleChange} value={formData.jenisKelamin}>
+                <label htmlFor="jenisKelamin">Jenis kelamin</label>
+                <select name="jenisKelamin" id="jenisKelamin" className="form-control" onChange={handleChange} value={formData.jenisKelamin}>
                   <option value="">Pilih jenis kelamin</option>
                   <option value="Jantan">Jantan</option>
                   <option value="Betina">Betina</option>
                 </select>
               </div>
               <div className="form-group">
-                <label htmlFor="">Berat badan</label>
-                <input type="text" className="form-control" onChange={handleChange} name="berat" placeholder="Contoh: 3.5 kg" value={formData.berat} />
+                <label htmlFor="berat-badan">Berat badan</label>
+                <input type="text" id='berat-badan' className="form-control" onChange={handleChange} name="berat" placeholder="Contoh: 3.5 kg" value={formData.berat} />
               </div>
             </div>
           </div>
@@ -109,23 +111,23 @@ const AddPatient = () => {
             <h1 className="font-bold text-2xl">Informasi pemilik</h1>
             <div className="grid grid-cols-2 gap-3">
               <div className="form-group">
-                <label htmlFor="">Nama Pemilik</label>
-                <input type="text" className="form-control" name="namaPemilik" onChange={handleChange} placeholder="" value={formData.namaPemilik} />
+                <label htmlFor="namaPemilik">Nama Pemilik</label>
+                <input type="text" id='namaPemilik' className="form-control" name="namaPemilik" onChange={handleChange} placeholder="" value={formData.namaPemilik} />
               </div>
               <div className="form-group">
-                <label htmlFor="">Nomor telepon</label>
-                <input type="text" className="form-control" name="noTelp" onChange={handleChange} placeholder="Masukkan nomor telepon" value={formData.noTelp} />
+                <label htmlFor="noTelp">Nomor telepon</label>
+                <input type="text" id='noTelp' className="form-control" name="noTelp" onChange={handleChange} placeholder="Masukkan nomor telepon" value={formData.noTelp} />
               </div>
             </div>
             <div className="form-group ">
-              <label htmlFor="">Alamat</label>
-              <textarea type="text" name='alamat' className="control-area" onChange={handleChange} placeholder="Masukkan nomor telepon" value={formData.alamat} />
+              <label htmlFor="alamat">Alamat</label>
+              <textarea type="text" id='alamat' name='alamat' className="control-area" onChange={handleChange} placeholder="Masukkan alamat pemilik" value={formData.alamat} />
             </div>
             <div className="form-group ">
-              <label htmlFor="">Catatan tambahan</label>
-              <textarea type="text" name='catatan' className="control-area" onChange={handleChange} placeholder="Catatan khusus tentang hewan (opsional)" value={formData.catatan} />
+              <label htmlFor="catatan">Catatan tambahan</label>
+              <textarea type="text" id='catatan' name='catatan' className="control-area" onChange={handleChange} placeholder="Catatan khusus tentang hewan (opsional)" value={formData.catatan} />
             </div>
-            <button type='submit'>Simpan pasien</button>
+            <button type='submit' className='button-save'>Simpan pasien</button>
             <button  className="ml-4">Reset form</button>
           </div>
         </div>
