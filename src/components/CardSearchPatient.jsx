ce892381002fe57945352ba842d43e76b76f7217
rivazahra <riva.zahra88@gmail.com>
2025-07-28 09:08:12 +0700

    Feat style for card search

--- src/components/CardSearchPatient.jsx
+++ src/components/CardSearchPatient.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const CardSearchPatient = ({ pasienData }) => {
   return (
-    <div className="space-y-5 ">
-        <div className="information-search space-y-3 flex items-center justify-between" >
+    <div className="space-y-5 text-sm ">
+        <div className="information-search max-md:flex-col max-md:items-start space-y-3 flex items-center justify-between" >
           <div>
-            <h1 className="name-patient font-bold capitalize">
+            <h1 className=" name-patient font-bold capitalize">
               {pasienData.name} - {pasienData.ras}
             </h1>
             <p className="text-sm text-gray-500">
@@ -15,9 +15,9 @@ const CardSearchPatient = ({ pasienData }) => {
               Umur: {pasienData.umur} tahun | Berat: {pasienData.berat} kg | Terakhir diperiksa: 15 Juli 2025
             </p>
           </div>
-          <div className="flex gap-4">
-            <button>Lihat Detail</button>
-            <button>Rekam medis</button>
+          <div className={`flex gap-4 max-md:flex-col`}>
+            <button className='btn cursor-pointer'>Lihat Detail</button>
+            <button className='p-2 bg-gray-200 font-semibold cursor-pointer rounded-md'>Rekam medis</button>
           </div>
         </div>
     </div>
