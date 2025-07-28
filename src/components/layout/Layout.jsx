618a354e0f7dec3c29528006abd18cb0b5b726d7
rivazahra <riva.zahra88@gmail.com>
2025-07-28 12:00:22 +0700

    Feat collapse when click

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -7,18 +7,14 @@ const Layout = () => {
   const [ collapsed, setIsCollapsed] = useState(false)
   const handleClick  = ()=>{
     setIsCollapsed(!collapsed)
-    console.log(
-      'klik'
-    );
-    
   }
   return (
     <ProtectedRoute>
       <div className="flex bg-[#F5F7FA] ">
-        <Sidebar collapsed={collapsed} onToogle={
+        <Sidebar collapsed={collapsed} setCollapse={setIsCollapsed} onToogle={
        handleClick
         } />
-        <main className={`flex-1 transition-all duration-300 min-h-screen  ${collapsed ? 'ml-12' : 'ml-72'}   overflow-y-auto`}>
+        <main className={`flex-1 ml-12 transition-all duration-300 min-h-screen ${collapsed ? 'md:ml-12' : 'md:ml-72'}   overflow-y-auto`}>
           <Outlet />
         </main>
       </div>
