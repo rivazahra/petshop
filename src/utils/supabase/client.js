687dadd9ad3ed61f0d604d13873821f3fd2f87f7
rivazahra <riva.zahra88@gmail.com>
2025-08-05 08:13:53 +0700

    Feat: fetch patient data from  Supabase instead of local storage

--- src/utils/supabase/client.js
+++ src/utils/supabase/client.js
@@ -1,7 +1,24 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
-export const supabase= createClient(supabaseUrl, supabaseAnonKey)
+export const getPatient = async () => {
+  const { data, error } = await supabase.from('pets').select(`
+      *,
+      owners (
+        name,
+        email,
+        phone
+      )
+    `)
+  if (error) {
+    console.log('Error fetching pets:', error)
+    return null
+  }
+  console.log('data',data);
+  
+  return data
+}
